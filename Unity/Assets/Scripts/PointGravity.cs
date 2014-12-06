ceffbd2a24365d0af058f8db62ff60070b3be625
Marcus Ihde <marcusplate@googlemail.com>
2014-12-06 22:15:26 +0100

    Add sample planet script.

--- Unity/Assets/Scripts/PointGravity.cs
+++ Unity/Assets/Scripts/PointGravity.cs
@@ -2,30 +2,14 @@
 using System.Collections;
 
 public class PointGravity : MonoBehaviour {
-	public Transform gravitySource;
-	public float gravScalar = 4.0f;
-	public Vector3 rotOffset = Vector3.zero;
-	// Use this for initialization
-	void Start () {
+	void OnTriggerStay (Collider other) {
+		other.attachedRigidbody.useGravity = false;
 		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		var direction = gravitySource.position - transform.position;
-		rigidbody.AddForce(direction.normalized * gravScalar);
-		transform.rotation = Quaternion.LookRotation(direction.normalized) * Quaternion.Euler(rotOffset);
-		if (Input.GetKey (KeyCode.W)) {
-			rigidbody.AddForce(transform.forward * 5f);
-		}
-		else if (Input.GetKey (KeyCode.S)) {
-			rigidbody.AddForce(transform.forward * -5f);
-		}
-		if (Input.GetKey (KeyCode.A)) {
-			rigidbody.AddForce(transform.right * -5f);
-		}
-		else if (Input.GetKey (KeyCode.D)) {
-			rigidbody.AddForce(transform.right * 5f);
+		var direction = -(other.attachedRigidbody.transform.position - transform.position);
+		
+		if (other.attachedRigidbody)
+		{
+			other.attachedRigidbody.constantForce.force = direction;
 		}
 	}
 }
---
+++ Unity/Assets/changeRoom.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeRoom : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider coll) {
+		if (coll.gameObject.name == "Capsule") {
+			Debug.Log("trig");
+			GameObject.Find("SphereCube").transform.Rotate(new Vector3(0,90,0));
+		}
+	}
+}
