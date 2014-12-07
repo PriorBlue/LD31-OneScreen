a6d49eb79912c6c1c57e70b1205d9dcd1a0d1e73
Marcus Ihde <marcusplate@googlemail.com>
2014-12-07 21:28:30 +0100

    Add Trigger System.

---
+++ Unity/Assets/openDoor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openDoor : MonoBehaviour {
+	public int doorID = 0;
+	public string animationName = "openDoor";
+
+	void OnTriggerEnter(Collider coll) {
+		Debug.Log (coll.gameObject.name);
+		if(coll.gameObject.name != "Player" && coll.gameObject.name != "Pusher") {
+			GameObject.Find ("Door" + doorID).animation.Play (animationName);
+		}
+	}
+}
