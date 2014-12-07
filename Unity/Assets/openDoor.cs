0f991ddaeb2544fb97b224aaf13b792e6f1d8c1c
Marcus Ihde <marcusplate@googlemail.com>
2014-12-07 21:43:04 +0100

    Add push buttons.

--- Unity/Assets/openDoor.cs
+++ Unity/Assets/openDoor.cs
@@ -4,11 +4,22 @@ using System.Collections;
 public class openDoor : MonoBehaviour {
 	public int doorID = 0;
 	public string animationName = "openDoor";
-
+	public bool floor = true;
+	private bool opened = false;
+	
 	void OnTriggerEnter(Collider coll) {
-		Debug.Log (coll.gameObject.name);
-		if(coll.gameObject.name != "Player" && coll.gameObject.name != "Pusher") {
-			GameObject.Find ("Door" + doorID).animation.Play (animationName);
+		if (!opened) {
+			if (floor) {
+				if (coll.gameObject.name != "Player" && coll.gameObject.name != "Pusher") {
+					GameObject.Find ("Door" + doorID).animation.Play (animationName);
+					opened = true;
+				}
+			} else {
+				if (coll.gameObject.name == "Pusher") {
+					GameObject.Find ("Door" + doorID).animation.Play (animationName);
+					opened = true;
+				}
+			}
 		}
 	}
 }
