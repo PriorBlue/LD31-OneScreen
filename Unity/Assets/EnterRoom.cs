3b99c45344dfcd075b7ac7dfb586f24439b8e69e
Marcus Ihde <marcusplate@googlemail.com>
2014-12-07 02:33:50 +0100

    Planet work's wohoooo... :D

---
+++ Unity/Assets/EnterRoom.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterRoom : MonoBehaviour {
+
+	public Transform point;
+
+	void OnTriggerStay(Collider coll) {
+		if(coll.name == "Player") {
+			Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, point.position, Time.deltaTime * 2.0f);
+			Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, point.rotation, Time.deltaTime * 2.0f);
+		}
+	}
+}
--- Unity/Assets/Scripts/PointGravity.cs
+++ Unity/Assets/Scripts/PointGravity.cs
@@ -2,16 +2,24 @@
 using System.Collections;
 
 public class PointGravity : MonoBehaviour {
+	public Vector3 rotOffset = Vector3.zero;
+
 	void OnTriggerStay (Collider other) {
 		other.rigidbody.useGravity = false;
 		
-		var direction = transform.position - other.transform.position;
+		Vector3 direction = transform.position - other.transform.position;
+		float dist = Vector3.Distance(other.transform.position, transform.position);
 		
-		if (other.rigidbody)
+		if (other.rigidbody && other.gameObject.tag == "Gravity")
 		{
-			other.rigidbody.AddForce(direction * 10.0f);
-			other.transform.rotation = Quaternion.LookRotation(direction.normalized) * Quaternion.Euler(new Vector3(270,0,0));
-			//other.transform.rotation = Quaternion.
+			if(dist >= 6.0f) {
+				other.rigidbody.AddForce(direction * 10.0f);
+			} else {
+				//other.rigidbody.AddForce(direction * -1.0f);
+			}
+			//other.rigidbody.velocity = direction * 0.001f;
+			//other.transform.rotation = Quaternion.LookRotation(direction.normalized) * Quaternion.Euler(rotOffset);
+			other.transform.rotation = Quaternion.FromToRotation(-other.transform.up, direction.normalized) * other.transform.rotation;
 		}
 	}
 }
