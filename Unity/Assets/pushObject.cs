f55e07eef035033b5f3f45d6bec3d95ea2064f2b
Supersonic112 <supersonic112@live.de>
2014-12-10 01:28:11 +0100

    potential fix for preventing boxes from being moved to the side. The values (for maxPushAngle and for the artificial forward vector) may still need to be adapted.

--- Unity/Assets/pushObject.cs
+++ Unity/Assets/pushObject.cs
@@ -2,9 +2,18 @@
 using System.Collections;
 
 public class pushObject : MonoBehaviour {
+    // maxPushAngle can be set to private once a good value is found
+    public float maxPushAngle = 55f;
+    Vector3 otherToPlayer = new Vector3(0,0,0);
 	void OnTriggerStay (Collider other) {
 		if (other.rigidbody && other.gameObject.tag == "Gravity" && other.gameObject.name != "Player") {
-			other.rigidbody.AddForce(transform.forward * 7500.0f);
+            otherToPlayer = other.transform.position - transform.position;
+            // Enable the line below for testing:
+            //Debug.Log("Angle:"+Vector3.Angle(transform.forward, otherToPlayer));
+            if (Vector3.Angle(Vector3.Normalize(7*transform.forward-3*transform.up), otherToPlayer) < maxPushAngle)
+            {
+                other.rigidbody.AddForce(transform.forward * 7500.0f);
+            }
 		}
 	}
 }
